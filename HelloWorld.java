1a462e47585e43e69324e89936cb14d4efcc1502
EricKanguzu <ekkanguzu@gmail.com>
2021-05-23 20:22:10 -0700

    HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -8,3 +8,13 @@ print (Name)
 
 This is just an additional line for testing purposes
 
+names = ['Kanguzu','Kaitlyn','Ashah','Gauti','Billion','Automation']
+
+print(names)
+
+new_name = raw_input('What name would you like to add to the list?    ')
+
+names.append(new_name)
+
+print(names)
+
